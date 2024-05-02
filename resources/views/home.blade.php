e314fad5440fcde7774f3fb95b4ea458135a0dba
bamboozaa <chukadee@gmail.com>
2024-05-02 11:20:56 +0700

    PC Commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,17 +25,19 @@
                         </div>
                         <div>Users</div>
                     </div>
-                    <div class="dropdown">
+                    {{-- <div class="dropdown">
                         <button class="btn btn-transparent text-white p-0" type="button" data-coreui-toggle="dropdown"
                             aria-haspopup="true" aria-expanded="false">
                             <svg class="icon">
                                 <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-options"></use>
                             </svg>
                         </button>
-                        <div class="dropdown-menu dropdown-menu-end"><a class="dropdown-item" href="#">Action</a><a
-                                class="dropdown-item" href="#">Another action</a><a class="dropdown-item"
-                                href="#">Something else here</a></div>
-                    </div>
+                        <div class="dropdown-menu dropdown-menu-end">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </div> --}}
                 </div>
                 <div class="c-chart-wrapper mt-3 mx-3" style="height:70px;">
                     <canvas class="chart" id="card-chart1" height="70" width="224"
@@ -56,18 +58,21 @@
                                 </svg>)
                             </span> --}}
                         </div>
-                        <div>รายการสัญญาทั้งหมด</div>
+                        {{ __('รายการสัญญาทั้งหมด') }}
                     </div>
                     <div class="dropdown">
                         <button class="btn btn-transparent text-white p-0" type="button" data-coreui-toggle="dropdown"
                             aria-haspopup="true" aria-expanded="false">
-                            <svg class="icon">
+                            <i class="bi bi-three-dots-vertical"></i>
+                            {{-- <svg class="icon">
                                 <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-options"></use>
-                            </svg>
+                            </svg> --}}
                         </button>
-                        <div class="dropdown-menu dropdown-menu-end"><a class="dropdown-item" href="#">Action</a><a
-                                class="dropdown-item" href="#">Another action</a><a class="dropdown-item"
-                                href="#">Something else here</a></div>
+                        <div class="dropdown-menu dropdown-menu-end">
+                            <a class="dropdown-item" href="{{ route('contracts.index') }}">Action</a>
+                            {{-- <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a> --}}
+                        </div>
                     </div>
                 </div>
                 <div class="c-chart-wrapper mt-3 mx-3" style="height:70px;">
