eaafa6de735623d61dd3e0f2f34509c95922dfdf
bamboozaa <chukadee@gmail.com>
2024-05-29 15:41:12 +0700

    PC Commit

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -1,13 +1,17 @@
 @extends('layouts.app')
 
+@section('importcss')
+    @parent
+    {{ Html::style('css/custom.css') }}
+@stop
+
 @section('content')
-    <div class="card mb-4">
+    {{-- <div class="card mb-4">
         <div class="card-header">
             {{ __('My profile') }}
         </div>
 
-        {{-- <form action="{{ route('profile.update') }}" method="POST"> --}}
-        <form action="#" method="POST">
+        <form action="{{ route('profile.update') }}" method="POST">
             @csrf
             @method('PUT')
 
@@ -43,7 +47,7 @@
                     @enderror
                 </div>
 
-                {{-- <div class="input-group mb-3"><span class="input-group-text">
+                <div class="input-group mb-3"><span class="input-group-text">
                     <svg class="icon">
                       <use xlink:href="{{ asset('icons/coreui.svg#cil-lock-locked') }}"></use>
                     </svg></span>
@@ -54,23 +58,258 @@
                             {{ $message }}
                         </span>
                     @enderror
-                </div> --}}
+                </div>
 
-                {{-- <div class="input-group mb-4"><span class="input-group-text">
+                <div class="input-group mb-4"><span class="input-group-text">
                     <svg class="icon">
                       <use xlink:href="{{ asset('icons/coreui.svg#cil-lock-locked') }}"></use>
                     </svg></span>
                     <input class="form-control @error('password_confirmation') is-invalid @enderror" type="password"
                            name="password_confirmation" placeholder="{{ __('New password confirmation') }}" required>
-                </div> --}}
+                </div>
 
             </div>
 
-            {{-- <div class="card-footer">
+            <div class="card-footer">
                 <button class="btn btn-sm btn-primary" type="submit">{{ __('Submit') }}</button>
-            </div> --}}
+            </div>
 
         </form>
 
+    </div> --}}
+    <div class="card pt-4 mb-6 mb-xl-9">
+        <!--begin::Card header-->
+        <div class="card-header border-0">
+            <!--begin::Card title-->
+            <div class="card-title">
+                <h2>Profile</h2>
+            </div>
+            <!--end::Card title-->
+        </div>
+        <!--end::Card header-->
+
+        <!--begin::Card body-->
+        <div class="card-body pt-0 pb-5">
+            <!--begin::Table wrapper-->
+            <div class="table-responsive">
+                <!--begin::Table-->
+                <table class="table align-middle table-row-dashed gy-5" id="kt_table_users_login_session">
+                    <tbody class="fs-6 fw-semibold text-gray-600">
+                        <tr>
+                            <td>Name</td>
+                            <td>{{ old('name', auth()->user()->name) }}</td>
+                            <td class="text-end">
+                                <button type="button" class="btn btn-icon btn-active-light-primary w-30px h-30px ms-auto" data-bs-toggle="modal" data-bs-target="#kt_modal_update_email">
+                                    <i class="bi bi-pencil fs-5"><span class="path1"></span><span class="path2"></span></i>                            </button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Email</td>
+                            <td>{{ old('email', auth()->user()->email) }}</td>
+                            <td class="text-end">
+                                <button type="button" class="btn btn-icon btn-active-light-primary w-30px h-30px ms-auto" data-bs-toggle="modal" data-bs-target="#kt_modal_update_email">
+                                    <i class="bi bi-pencil fs-5"><span class="path1"></span><span class="path2"></span></i>                            </button>
+                            </td>
+                        </tr>
+                        {{-- <tr>
+                            <td>Password</td>
+                            <td>******</td>
+                            <td class="text-end">
+                                <button type="button" class="btn btn-icon btn-active-light-primary w-30px h-30px ms-auto" data-bs-toggle="modal" data-bs-target="#kt_modal_update_password">
+                                    <i class="bi bi-pencil fs-5"><span class="path1"></span><span class="path2"></span></i>                            </button>
+                            </td>
+                        </tr> --}}
+                        <tr>
+                            <td>Role</td>
+                            <td>{{ auth()->user()->role === 1 ? "Administrator" : "User" }}</td>
+                            <td class="text-end">
+                                <button type="button" class="btn btn-icon btn-active-light-primary w-30px h-30px ms-auto" data-bs-toggle="modal" data-bs-target="#kt_modal_update_role">
+                                    <i class="bi bi-pencil fs-5"><span class="path1"></span><span class="path2"></span></i>                            </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <!--end::Table-->
+            </div>
+            <!--end::Table wrapper-->
+        </div>
+        <!--end::Card body-->
     </div>
+
+    <div class="modal fade" id="kt_modal_update_role" tabindex="-1" aria-hidden="true" style="display: none;">
+        <!--begin::Modal dialog-->
+        <div class="modal-dialog modal-dialog-centered mw-650px">
+            <!--begin::Modal content-->
+            <div class="modal-content">
+                <!--begin::Modal header-->
+                <div class="modal-header">
+                    <!--begin::Modal title-->
+                    <h2 class="fw-bold">Update User Role</h2>
+                    <!--end::Modal title-->
+
+                    <!--begin::Close-->
+                    <div class="btn btn-icon btn-sm btn-active-icon-primary" data-kt-users-modal-action="close">
+                        <i class="ki-duotone ki-cross fs-1"><span class="path1"></span><span class="path2"></span></i>                </div>
+                    <!--end::Close-->
+                </div>
+                <!--end::Modal header-->
+
+                <!--begin::Modal body-->
+                <div class="modal-body scroll-y mx-5 mx-xl-15 my-7">
+                    <!--begin::Form-->
+                    <form id="kt_modal_update_role_form" class="form" action="#">
+                        <!--begin::Notice-->
+
+    <!--begin::Notice-->
+    <div class="notice d-flex bg-light-primary rounded border-primary border border-dashed mb-9 p-6">
+                <!--begin::Icon-->
+            <i class="ki-duotone ki-information fs-2tx text-primary me-4"><span class="path1"></span><span class="path2"></span><span class="path3"></span></i>        <!--end::Icon-->
+
+        <!--begin::Wrapper-->
+        <div class="d-flex flex-stack flex-grow-1 ">
+                        <!--begin::Content-->
+                <div class=" fw-semibold">
+
+                                        <div class="fs-6 text-gray-700 ">Please note that reducing a user role rank, that user will lose all priviledges that was assigned to the previous role.</div>
+                                </div>
+                <!--end::Content-->
+
+                </div>
+        <!--end::Wrapper-->
+    </div>
+    <!--end::Notice-->
+                        <!--end::Notice-->
+
+                        <!--begin::Input group-->
+                        <div class="fv-row mb-7">
+                            <!--begin::Label-->
+                            <label class="fs-6 fw-semibold form-label mb-5">
+                                <span class="required">Select a user role</span>
+                            </label>
+                            <!--end::Label-->
+
+                                                        <!--begin::Input row-->
+                                <div class="d-flex">
+                                    <!--begin::Radio-->
+                                    <div class="form-check form-check-custom form-check-solid">
+                                        <!--begin::Input-->
+                                        <input class="form-check-input me-3" name="user_role" type="radio" value="0" id="kt_modal_update_role_option_0" checked="checked">
+                                        <!--end::Input-->
+
+                                        <!--begin::Label-->
+                                        <label class="form-check-label" for="kt_modal_update_role_option_0">
+                                            <div class="fw-bold text-gray-800">Administrator</div>
+                                            <div class="text-gray-600">Best for business owners and company administrators</div>
+                                        </label>
+                                        <!--end::Label-->
+                                    </div>
+                                    <!--end::Radio-->
+                                </div>
+                                <!--end::Input row-->
+
+                                <div class="separator separator-dashed my-5"></div>                                                    <!--begin::Input row-->
+                                <div class="d-flex">
+                                    <!--begin::Radio-->
+                                    <div class="form-check form-check-custom form-check-solid">
+                                        <!--begin::Input-->
+                                        <input class="form-check-input me-3" name="user_role" type="radio" value="1" id="kt_modal_update_role_option_1">
+                                        <!--end::Input-->
+
+                                        <!--begin::Label-->
+                                        <label class="form-check-label" for="kt_modal_update_role_option_1">
+                                            <div class="fw-bold text-gray-800">Developer</div>
+                                            <div class="text-gray-600">Best for developers or people primarily using the API</div>
+                                        </label>
+                                        <!--end::Label-->
+                                    </div>
+                                    <!--end::Radio-->
+                                </div>
+                                <!--end::Input row-->
+
+                                <div class="separator separator-dashed my-5"></div>                                                    <!--begin::Input row-->
+                                <div class="d-flex">
+                                    <!--begin::Radio-->
+                                    <div class="form-check form-check-custom form-check-solid">
+                                        <!--begin::Input-->
+                                        <input class="form-check-input me-3" name="user_role" type="radio" value="2" id="kt_modal_update_role_option_2">
+                                        <!--end::Input-->
+
+                                        <!--begin::Label-->
+                                        <label class="form-check-label" for="kt_modal_update_role_option_2">
+                                            <div class="fw-bold text-gray-800">Analyst</div>
+                                            <div class="text-gray-600">Best for people who need full access to analytics data, but don't need to update business settings</div>
+                                        </label>
+                                        <!--end::Label-->
+                                    </div>
+                                    <!--end::Radio-->
+                                </div>
+                                <!--end::Input row-->
+
+                                <div class="separator separator-dashed my-5"></div>                                                    <!--begin::Input row-->
+                                <div class="d-flex">
+                                    <!--begin::Radio-->
+                                    <div class="form-check form-check-custom form-check-solid">
+                                        <!--begin::Input-->
+                                        <input class="form-check-input me-3" name="user_role" type="radio" value="3" id="kt_modal_update_role_option_3">
+                                        <!--end::Input-->
+
+                                        <!--begin::Label-->
+                                        <label class="form-check-label" for="kt_modal_update_role_option_3">
+                                            <div class="fw-bold text-gray-800">Support</div>
+                                            <div class="text-gray-600">Best for employees who regularly refund payments and respond to disputes</div>
+                                        </label>
+                                        <!--end::Label-->
+                                    </div>
+                                    <!--end::Radio-->
+                                </div>
+                                <!--end::Input row-->
+
+                                <div class="separator separator-dashed my-5"></div>                                                    <!--begin::Input row-->
+                                <div class="d-flex">
+                                    <!--begin::Radio-->
+                                    <div class="form-check form-check-custom form-check-solid">
+                                        <!--begin::Input-->
+                                        <input class="form-check-input me-3" name="user_role" type="radio" value="4" id="kt_modal_update_role_option_4">
+                                        <!--end::Input-->
+
+                                        <!--begin::Label-->
+                                        <label class="form-check-label" for="kt_modal_update_role_option_4">
+                                            <div class="fw-bold text-gray-800">Trial</div>
+                                            <div class="text-gray-600">Best for people who need to preview content data, but don't need to make any updates</div>
+                                        </label>
+                                        <!--end::Label-->
+                                    </div>
+                                    <!--end::Radio-->
+                                </div>
+                                <!--end::Input row-->
+
+                                                                            </div>
+                        <!--end::Input group-->
+
+                        <!--begin::Actions-->
+                        <div class="text-center pt-15">
+                            <button type="reset" class="btn btn-light me-3" data-kt-users-modal-action="cancel">
+                                Discard
+                            </button>
+
+                            <button type="submit" class="btn btn-primary" data-kt-users-modal-action="submit">
+                                <span class="indicator-label">
+                                    Submit
+                                </span>
+                                {{-- <span class="indicator-progress">
+                                    Please wait... <span class="spinner-border spinner-border-sm align-middle ms-2"></span>
+                                </span> --}}
+                            </button>
+                        </div>
+                        <!--end::Actions-->
+                    </form>
+                    <!--end::Form-->
+                </div>
+                <!--end::Modal body-->
+            </div>
+            <!--end::Modal content-->
+        </div>
+        <!--end::Modal dialog-->
+    </div>
+
 @endsection
