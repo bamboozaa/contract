80b62cbeb4246e2622b4d9bf0886012f0866095c
bamboozaa <chukadee@gmail.com>
2025-03-14 17:09:33 +0700

    PC Commit

--- app/Http/Controllers/ContractController.php
+++ app/Http/Controllers/ContractController.php
@@ -108,7 +108,7 @@ class ContractController extends Controller
             try {
                 $response = Http::withHeaders([
                     'Content-Type' => 'application/json',
-                ])->post('http://10.7.45.123/api/contractslegal/' . $contractId . '?check=RCM', $body);
+                ])->post('http://10.7.45.125/api/contractslegal/' . $contractId . '?check=RCM', $body);
 
                 // Access the response
                 $statusCode = $response->status();
@@ -191,7 +191,7 @@ class ContractController extends Controller
 
             $contractId = $request->input('contractid');
 
-            $url = 'http://10.7.45.123/api/contractslegal/' . $contractId . '?check=RCM';
+            $url = 'http://10.7.45.125/api/contractslegal/' . $contractId . '?check=RCM';
             $body = [
                 "legalID" => "นตก.(ส)" . " " . $request->input('contract_no') . "/" . $request->input('contract_year'),
                 "signDate" => $request->input('contract_date'),
